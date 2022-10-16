9b804a53a6423d7ad3e59cc70961b26e1ea421d3
Lasanthi22 <113050130+Lasanthi22@users.noreply.github.com>
2022-10-16 15:00:01 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,8 +54,8 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
+global.vcardowner = ['94 77 479 3303'] //ur owner number
+global.ownername = "𝚜𝚎𝚑𝚊𝚜 " //ur owner name
 global.ytname = "YT: Xeon" //ur yt chanel name
 global.socialm = "GitHub: DGXeon" //ur github or insta name
 global.location = "India, Mizoram, Aizawl" //ur location
